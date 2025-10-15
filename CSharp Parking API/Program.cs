a76169ba6a9748f5148273a5837776d1bfcb3919
Lombex <LombexTV@gmail.com>
2025-10-15 10:35:18 +0200

    Fixed Main, added the cors and fixed up scopes

--- CSharp Parking API/Program.cs
+++ CSharp Parking API/Program.cs
@@ -1,6 +1,31 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CSharpAPI.Database;
+using CSharpAPI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+builder.Services.AddDbContext<SQLite_Database>(options => 
+   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUsersService, S_Users>();
+// Add other services here
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -8,8 +33,23 @@ app.UseRouting();
 app.UseCors("AllowAll");
 // Midelware
 // Authorization
-// app.MapControllers();
+app.MapControllers();
 
 app.Urls.Add("http://localhost:5001");
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<SQLite_Database>();
+    // authentication
+    // logger 
+    try
+    {
+        await dbContext.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred while migrating the database: {ex.Message}");
+    }
+}
 
 app.Run();
