2988d6367342a4319aeb4bc452b97482f034d30f
catlovescake <IremP23@outlook.com>
2025-11-06 11:10:01 +0100

    update parkinglots
    parkinglots up until now

---
+++ CSharp Parking API/Models/Model_Parkinglots.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CSharpAPI.Models
+{
+    public class M_Parkinglots
+    {
+        public Guid id { get; set; }
+        public string? name { get; set; }
+        public string? location { get; set; }
+        public string? address { get; set; }
+        public int capacity { get; set; }
+        public int reserved { get; set; }
+        public float daytarriff { get; set; }
+        public DateTime created_at { get; set; }
+        public Coordinates? coordinates { get; set; }
+    }
+    public class M_Tariff
+    {
+        public Guid id { get; set; }
+        public Guid parkinglotId { get; set; }
+        public List<Tariff> tariffs { get; set; }
+    }
+
+    public class M_ParkinglotRates
+    {
+        public Guid id { get; set; }
+        public Guid parkinglotId { get; set; }
+        public List<M_Tariff> tariffs { get; set; }
+    }
+    [Owned]
+    public class Coordinates
+    {
+        public float lat { get; set; }
+        public float lng { get; set; }
+
+    }
+    [Owned]
+    public class Tariff
+    {
+        public Guid id { get; set; }
+        public string? type { get; set; }
+        public decimal price { get; set; }
+    }
+}
